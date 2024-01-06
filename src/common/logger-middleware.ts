4a03828fb319bf8652fe56d02e1dc0bdead1d248
VuAnhQuan-alh <quanva2521@gmail.com>
2024-01-06 15:41:03 +0700

    fix: update base

---
+++ src/common/logger-middleware.ts
@@ -0,0 +1,33 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class AppLoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
+
+  use(request: Request, response: Response, next: NextFunction) {
+    const { ip, method, baseUrl } = request;
+    const userAgent = request.get('user-agent') || '';
+    response.on('close', () => {
+      const { statusCode } = response;
+      const contentLength = request.get('content-length') || 0;
+
+      if (
+        statusCode !== 200 &&
+        statusCode !== 201 &&
+        statusCode !== 304 &&
+        statusCode !== 302
+      ) {
+        this.logger.error(
+          `${method} ${statusCode} ${baseUrl} ${contentLength} - ${userAgent} ${ip}`,
+        );
+      } else {
+        this.logger.log(
+          `${method} ${statusCode} ${baseUrl} ${contentLength} - ${userAgent} ${ip}`,
+        );
+      }
+    });
+
+    next();
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.use(helmet());
+  app.setGlobalPrefix('api/v1');
+  await app.listen(process.env.PORT);
+
+  console.log(`Application is running on: ${await app.getUrl()}/api/v1`);
 }
 bootstrap();
